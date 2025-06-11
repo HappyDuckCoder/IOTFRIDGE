91b3cd0ee1699abc333e76087359289c18fc8160
duckilot <prto2802@gmail.com>
2025-06-11 11:37:03 +0700

    pull and merge

--- thirdparty/database/model.py
+++ thirdparty/database/model.py
@@ -1,2 +1,29 @@
-# class Food 
+﻿# class Food 
 # --> id, name, quantity, unit, image=null, state 
+
+class Food:
+    def __init__(self, id, name, quantity, unit, status):
+        self.id = id
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+        self.status = status  # "good" hoặc "expired"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "quantity": self.quantity,
+            "unit": self.unit,
+            "status": self.status
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Food(
+            id=data.get("id"),
+            name=data.get("name"),
+            quantity=data.get("quantity"),
+            unit=data.get("unit"),
+            status=data.get("status")
+        )
--- thirdparty/database/testMethodDatabase.py
+++ thirdparty/database/testMethodDatabase.py
@@ -1,1 +1,18 @@
-# test code here
+﻿# test code here
+
+from email.utils import formataddr
+from model import Food
+from method import add_food, get_all_foods
+
+# Thêm đồ ăn mới
+new_food = Food(id="banana1", name="Chuối", quantity=3, unit="trái", status="còn tươi")
+add_food(new_food)
+food_1 = Food(id="apple1", name="Táo", quantity=5, unit="trái", status="còn tươi")
+add_food(food_1)
+food_2 = Food(id="orange1", name="Cam", quantity=2, unit="trái", status="bị hư")
+add_food(food_2)
+
+# In toàn bộ đồ ăn
+foods = get_all_foods()
+for f in foods:
+    print(f"{f.name}: {f.quantity} {f.unit} \nTrạng thái: {f.status}")
