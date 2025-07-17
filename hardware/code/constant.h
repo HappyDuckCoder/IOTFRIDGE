a5a5274b3079990dd9ce7c71fa0774aed22e16d1
TBA54 <thaibaoprovip@gmail.com>
2025-07-17 16:17:52 +0700

    Update Relay & DHT & TFT

--- hardware/code/constant.h
+++ hardware/code/constant.h
@@ -1,4 +1,4 @@
-#ifndef CONSTANT_H
+﻿#ifndef CONSTANT_H
 #define CONSTANT_H
 
 #define MQ2_PIN 34
@@ -9,5 +9,10 @@
 #define INMP_SCK 22
 
 #define RECORD_BUTTON 19
+
+#define FAN_BUTTON 4        // Pin nút điều khiển quạt
+#define DHT_PIN 5           // Pin cảm biến DHT22
+#define RELAY_PIN 6         // Pin relay điều khiển quạt
+#define PWM_PIN 7           // Pin PWM điều khiển tốc độ quạt
 
 #endif
