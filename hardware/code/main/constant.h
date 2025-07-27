2326bffbb02c5026c056f82aed53aeb6a3955878
duckilot <prto2802@gmail.com>
2025-07-28 00:10:58 +0700

    tft

--- hardware/code/main/constant.h
+++ hardware/code/main/constant.h
@@ -2,20 +2,30 @@
 #define CONSTANT_H
 
 // Button
-#define BUTTON_MIC_PIN    16
-#define BUTTON_FAN_PIN    17
-#define BUTTON_DOOR_PIN   18
+#define BUTTON_MIC_PIN      16
+#define BUTTON_FAN_PIN      17
+#define BUTTON_DOOR_PIN     18
 
 // DHT
-#define DHT_PIN           19
-#define DHT_TYPE          DHT11  
+#define DHT_PIN             19
+#define DHT_TYPE            DHT11  
 
 // MQ
-#define MQ2_PIN           32
-#define MQ135_PIN         33
+#define MQ2_PIN             32
+#define MQ135_PIN           33
 
 // relay 
-#define RELAY_PIN         23
+#define RELAY_PIN           21
+
+// TFT 
+#define TFT_CS_PIN          5
+#define TFT_DC_PIN          2
+#define TFT_RST_PIN         4
+#define TFT_SCLK_PIN        22
+#define TFT_MOSI_PIN        23
+#define TFT_SCREEN_WIDTH    240
+#define TFT_SCREEN_HEIGHT   320
+
 
 
 #endif 
