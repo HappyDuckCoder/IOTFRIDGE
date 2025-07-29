6bcbef8d044dd69b1c3455186203a3baf24599cf
duckilot <prto2802@gmail.com>
2025-07-29 20:32:03 +0700

    start test

--- docs/sampleCodeHardwareAndDocument/sample/SampleTestDoorChecking.cpp
+++ docs/sampleCodeHardwareAndDocument/sample/SampleTestDoorChecking.cpp
@@ -4,26 +4,31 @@
 // Pin setup
 #define BUTTON_PIN 25
 #define HX711_DOUT 26
-#define HX711_SCK  27
+#define HX711_SCK 27
 
 DoorTracking door(BUTTON_PIN);
 WeightTracking weight(HX711_DOUT, HX711_SCK);
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     door.begin();
     weight.begin();
 }
 
-void loop() {
-    if (door.isAlertNeeded()) {
+void loop()
+{
+    if (door.isAlertNeeded())
+    {
         Serial.println("ALERT: Door open too long!");
         // TODO: Gửi thông báo lên server
     }
 
-    if (door.isDoorJustClosed()) {
+    if (door.isDoorJustClosed())
+    {
         Serial.println("Door just closed. Checking weight...");
-        if (weight.checkWeightChange()) {
+        if (weight.checkWeightChange())
+        {
             Serial.println("Weight changed significantly!");
             // TODO: Gửi thông báo yêu cầu nhập/chỉnh sửa thực phẩm trên website
         }
@@ -32,9 +37,10 @@ void loop() {
     delay(200);
 }
 
+// GPIO 25 → Button(door sensor).
 
-GPIO 25 → Button (door sensor).
+//     GPIO 26 → HX711 Data(DOUT)
+//         .
 
-GPIO 26 → HX711 Data (DOUT).
-
-GPIO 27 → HX711 Clock (SCK).
+//     GPIO 27 → HX711 Clock(SCK)
+//         .
