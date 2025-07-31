f4cfe2b831fd594491f7709b1b88f32455b5e535
duckilot <prto2802@gmail.com>
2025-07-31 21:01:28 +0700

    xong settiing

--- server/TestingMethod.py
+++ server/TestingMethod.py
@@ -1,6 +1,6 @@
-from thirdparty.database.model import Food, Condition
+from thirdparty.database.model import Food, Condition, Setting
 from thirdparty.api.GetCalo import GetCaloService
-from thirdparty.database.method import add_food, get_all_foods, delete_food_by_id, delete_all_foods, add_fridge_conditions
+from thirdparty.database.method import add_food, get_all_foods, delete_food_by_id, delete_all_foods, add_fridge_conditions, add_setting, delete_setting_by_id, get_setting_by_id, update_setting_by_id
 from my_util import get_category, get_image_url
 from datetime import datetime, timedelta
 
@@ -70,8 +70,8 @@ def test_delete_food_by_id():
     delete_food_by_id("2")  # Xóa chuối
 
 def food_all_test():
-    # delete_all_foods_()
-    # test_add_food()
+    delete_all_foods_()
+    test_add_food()
     test_get_all_foods()
 
 def add_condition(): 
@@ -79,5 +79,35 @@ def add_condition():
     sample_condition = Condition("1", now, 0.0, 0.0, False, 0, "1")
     add_fridge_conditions(sample_condition)
 
+def test_setting():
+    sample_setting = Setting(
+        id="2", 
+        diet="healthy",
+        fan_mode=1,
+        is_open_notification=False,
+        fridge_data_id="1"
+    )
+
+    # Thêm setting
+    add_setting(sample_setting)
+    print("Đã thêm setting.")
+
+    # Cập nhật fan_mode
+    update_setting_by_id("2", {"fan_mode": 0})
+    print("Đã cập nhật fan_mode.")
+
+    # Lấy lại để kiểm tra
+    get_setting = get_setting_by_id("2")
+    print("Dữ liệu sau cập nhật:", get_setting.to_dict() if get_setting else "Không tìm thấy")
+
+    # Xoá
+    delete_setting_by_id("2")
+    print("Đã xoá setting.")
+
+    # Kiểm tra lại sau xoá
+    get_setting = get_setting_by_id("2")
+    print("Kết quả sau khi xoá:", get_setting if get_setting else "Không tồn tại")
+
+
 if __name__ == "__main__":
-    food_all_test()
+    test_setting()
